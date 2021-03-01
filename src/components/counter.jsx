632a63800ea23f46f38a28af5dd115d8dfc560bd
Aditi Dable <aditidable01@gmail.com>
2021-03-01 20:53:06 +0530

    destructuring Arguements

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,42 +1,46 @@
 import React, { Component } from 'react';
 class Counter extends Component {
-     state = { value : this.props.value
-        //tags:[]
-    };
+    //  state = { value : this.props.counter.value
+    //     //tags:[]
+    // };
     // constructor()
     // {  super();
     //     this.handleIncrement= this.handleIncrement.bind(this);
     //     //console.log('constructor',this);
     // }
 
-     handleIncrement=()=>
-     {
-         console.log("Increment counter", this);
-         this.setState({value: this.state.value+1});
-     }
+    //  handleIncrement=()=>
+    //  {
+    //      console.log("Increment counter", this);
+    //      this.setState({value: this.state.value+1});
+    //  }
      
     
     render() { 
         //className={this.getBadgeClasses()}
-        console.log("props",this.props);
+        
         return (
-        <React.Fragment>
           
+        <React.Fragment>
+          {this.props.children}
             <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-          <button className="btn btn-secondary" onClick={this.handleIncrement}>
+          <button className="btn btn-secondary" onClick={()=> this.props.onIncrement(this.props.counter)}>
           increment
           </button>
+          <button onClick={()=> this.props.onDelete(this.props.counter.id)} 
+          className="btn btn-danger btn-sm m-2">Delete</button>
+         
         </React.Fragment>
         );
     }
     getBadgeClasses(){
         let classes= "badge m-2 badge-";
-        classes += this.state.count === 0? "warning":"primary";
+        classes += this.props.counter.value === 0? "warning":"primary";
         return classes;
     }
      formatCount() {
-          const {count}=this.state;
-        return count===0? 'Zero':count; 
+          const {value}=this.props.counter;
+        return value===0? 'Zero':value; 
      }
     
 }
